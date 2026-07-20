612cf10e0658e8febd926e14118de83227ee1704
cser-utkarsh-raj <cser.utkarsh.raj@gmail.com>
2026-07-20 22:51:29 +0530

    fix: alert actual error in GoalSetup.tsx

--- frontend/src/pages/GoalSetup.tsx
+++ frontend/src/pages/GoalSetup.tsx
@@ -109,9 +109,9 @@ export const GoalSetup: React.FC = () => {
         setIsGenerating(false)
         navigate('/app')
       }, 2500)
-    } catch (e) {
+    } catch (e: any) {
       setIsGenerating(false)
-      alert('Error creating your goal. Please verify backend service connection.')
+      alert(`Error creating your goal: ${e.message || e}`);
     }
   }
 
